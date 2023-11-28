20ba921afb822807f8da7fa1e7bed97c1c181664
srilekhad <70757195+srilekhad@users.noreply.github.com>
2023-11-28 07:42:11 -0600

    Update add_to_cart.py

--- shoping_app/models/managers/add_to_cart.py
+++ shoping_app/models/managers/add_to_cart.py
@@ -10,7 +10,7 @@ class AddToCartManager:
     async def add_product_to_cart(self, name: str, user_id):
 
         product = await Products.Model.objects.find_product(name)
-        if not db.find_one({"name": name}):
+        if not db.find_one({"name": name, "user_id": user_id}):
             product['user_id'] = user_id
             product['quantity'] = 1
             db.insert_one(product)
